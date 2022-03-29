5855c65fa23b45ce921b1eb5337ee14ba88b323d
Mathew Layne <akeemolayne@gmail.com>
2022-03-29 13:51:16 -0500

    migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,8 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('user_type');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
