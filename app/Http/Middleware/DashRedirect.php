c9da883428a1b70fa474647683a477df8dfd6ce7
Mathew Layne <akeemolayne@gmail.com>
2022-03-30 11:11:48 -0500

    middleware update

--- app/Http/Middleware/DashRedirect.php
+++ app/Http/Middleware/DashRedirect.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashRedirect
 {
@@ -16,6 +17,14 @@ class DashRedirect
      */
     public function handle(Request $request, Closure $next)
     {
+        if(Auth::user()->user_type == "Admin"){
+            return redirect()->route('admin.dashboard');
+        }
+
+        if(Auth::user()->user_type == "Parent"){
+            return redirect()->route('parents.voucher');
+        }
+
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'dashredirect'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
