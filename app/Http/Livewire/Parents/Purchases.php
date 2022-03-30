15e8b7c2ceb90065de040611b9d9c9d8538b9b2a
Mathew Layne <akeemolayne@gmail.com>
2022-03-30 13:45:21 -0500

    purchases update

--- app/Http/Livewire/Parents/Purchases.php
+++ app/Http/Livewire/Parents/Purchases.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Parents;
 
+use App\Models\Purchase;
+use App\Models\VoucherHistory;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Purchases extends Component
 {
     public function render()
-    {
-        return view('livewire.parents.purchases');
+    {  
+        $purchases = Purchase::where('parent_id', Auth::id())->get();
+
+        return view('livewire.parents.purchases', [
+            'purchases' => $purchases
+        ]);
     }
 }
--- resources/views/livewire/parents/purchases.blade.php
+++ resources/views/livewire/parents/purchases.blade.php
@@ -12,34 +12,27 @@
             <span class="text-gray-300">Price</span>
           </th>
           <th class="px-16 py-2">
-            <span class="text-gray-300">Actions</span>
+            <span class="text-gray-300">Purchase Date</span>
           </th>
         </tr>
       </thead>
       <tbody class="bg-gray-200">
-        {{-- @foreach ($vouchers as $voucher) --}}
+        @foreach ($purchases as $purchase)
         <tr class="bg-white border-4 border-gray-200">
           <td class="">
-            <span class="text-center ml-2 font-semibold">1</span>
+            <span class="text-center ml-2 font-semibold">{{ $purchase->voucher->name }}</span>
           </td>
           <td class="px-16 py-2">
-            <span>1</span>
+            <span>${{ $purchase->voucher->value }}.00</span>
           </td>
           <td class="px-16 py-2">
-            <span>1</span>
+            <span>${{ $purchase->voucher->price }}</span>
           </td>
           <td class="px-16 py-2">
-            <button type="button" 
-              class="border border-indigo-500 bg-black text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
-              Edit
-            </button>
-            <button type="button" 
-              class="border border-indigo-500 bg-black text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
-              Delete
-            </button>
+            <span>{{ $purchase->purchase_date }}</span>            
           </td>
         </tr>
-        {{-- @endforeach --}}
+        @endforeach
       </tbody>
     </table>
   </div>
